b2e2d673a13938f2825f7d3b0d9ac74f143ce932
emilianoagustin <pintos.emiliano@gmail.com>
2021-07-07 00:39:45 -0300

    added header component and hooks to fetch data from api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Header from '../src/components/Header/Header.js';
 import './App.css';
+const URL = "https://www.breakingbadapi.com/api/";
 
-function App() {
+const App = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(URL + "characters")
+      setItem(response.data)
+      setIsLoading(false)
+    }
+
+    fetchData()
+  }, []);
+
   return (
-    <div className="App">
-      <h1>hello</h1>
+    <div className="container">
+      <Header />
     </div>
   );
 }
