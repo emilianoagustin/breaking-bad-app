d1b878442fe1b1243425ff2218c22254ae4f3aaf
emilianoagustin <pintos.emiliano@gmail.com>
2021-07-07 01:25:20 -0300

    created SearchBar component

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SearchBar = () => {
+	return (
+		<div>
+			<form action="">
+				<input type="text" />
+			</form>
+		</div>
+	)
+}
+
+export default SearchBar;
