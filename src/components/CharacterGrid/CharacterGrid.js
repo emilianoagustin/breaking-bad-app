becb9ed46a03cbb57cb53babaa78b81ed2a594b0
emilianoagustin <pintos.emiliano@gmail.com>
2021-07-07 01:56:30 -0300

    created Spinner component

--- src/components/CharacterGrid/CharacterGrid.js
+++ src/components/CharacterGrid/CharacterGrid.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import CharacterItem from '../CharacterItem/CharacterItem.js';
+import Spinner from '../Spinner/Spinner.js';
 
 const CharacterGrid = ({ items, isLoading }) => {
 	return (
 		isLoading ?
 		(
-			<div>
-			loading...
-			</div>
+			<Spinner />
 		):
 		(
 			<section className='cards'>
