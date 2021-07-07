38e62270cded394a3ab36f61f12752cb242fa3e9
emilianoagustin <pintos.emiliano@gmail.com>
2021-07-07 00:40:18 -0300

    created header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../../assets/logo.png';
+
+const Header = () => {
+	return (
+		<div className='center'>
+			<img src={logo} alt='logo-image'/>
+		</div>
+	)
+}
+
+export default Header;
